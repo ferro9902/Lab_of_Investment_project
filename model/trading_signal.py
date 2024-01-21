5d3370c5cdcf11573e911d1bf053c658f9fbd9be
ferro9902 <matrix.code99@gmail.com>
2024-01-21 20:38:36 +0100

    added comments and organizzed data cleaning code

--- model/trading_signal.py
+++ model/trading_signal.py
@@ -2,7 +2,6 @@
 def compute_trading_signal(data):
     # Create the 'trading_signal' column
     data['trading_signal'] = 0  # Default to 0
-    data['holding'] = 0  # Default to 0
     
     # Initialize variables to track the last non-zero trading signal
     last_non_zero_signal = 0
@@ -13,8 +12,6 @@ def compute_trading_signal(data):
             # Check if the last non-zero signal was -1
             if last_non_zero_signal == -1:
                 data.at[index, 'trading_signal'] = 1
-            else:
-                data.at[index, 'holding'] = 1
             last_non_zero_signal = 1
         elif not row['y']:
             # Check if the last non-zero signal was 1
