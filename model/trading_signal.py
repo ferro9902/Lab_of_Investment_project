9ee0068262f830325508001558484b574926afd0
ferro9902 <matrix.code99@gmail.com>
2024-01-21 18:00:39 +0100

    corretto grafico LS-SVM vs Buy and hold

--- model/trading_signal.py
+++ model/trading_signal.py
@@ -1,18 +1,8 @@
 
 def compute_trading_signal(data):
     # Create the 'trading_signal' column
-    
     data['trading_signal'] = 0  # Default to 0
-
-   # Apply conditions to set +1, 0, -1 to 'trading_signal'
-    data.loc[(data['y'].shift(1) != data['y']) & (data['y'] == True), 'trading_signal'] = 1
-    data.loc[(data['y'].shift(1) != data['y']) & (data['y'] == False), 'trading_signal'] = -1
-    return data
-
-"""
-def compute_trading_signal(data):
-    # Create the 'trading_signal' column
-    data['trading_signal'] = 0  # Default to 0
+    data['holding'] = 0  # Default to 0
     
     # Initialize variables to track the last non-zero trading signal
     last_non_zero_signal = 0
@@ -23,6 +13,8 @@ def compute_trading_signal(data):
             # Check if the last non-zero signal was -1
             if last_non_zero_signal == -1:
                 data.at[index, 'trading_signal'] = 1
+            else:
+                data.at[index, 'holding'] = 1
             last_non_zero_signal = 1
         elif not row['y']:
             # Check if the last non-zero signal was 1
@@ -31,4 +23,3 @@ def compute_trading_signal(data):
             last_non_zero_signal = -1
     
     return data
-"""
